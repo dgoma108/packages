1524a2e1e869ccf9e34b336603ccf9e6c9e37b80
David Gomà <dgoma108@gmail.com>
2021-09-06 09:01:10 +0200

    10 Sobreescribir vistas

--- src/FirstPackageServiceProvider.php
+++ src/FirstPackageServiceProvider.php
@@ -13,6 +13,10 @@ class FirstPackageServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
 
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'first-package');
+
+        $this->publishes([
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/first-package')
+        ], 'first-package-views');
     }
 
     public function register()
